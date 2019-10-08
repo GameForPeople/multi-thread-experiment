1d9f3b23805283506c8f06394f60b3f8267373b1
GameForPeople <koreagamemaker@gmail.com>
2019-10-08 22:11:52 +0900

    Add CustomList

--- MultiThreadExperiment/MultiThreadExperiment/CustomList.h
+++ MultiThreadExperiment/MultiThreadExperiment/CustomList.h
@@ -23,24 +23,6 @@ namespace GLOBAL
 
 }
 
-namespace GLOBAL_LIST_FUNCTION
-{
-	template <typename _List, typename _Node>
-	void Display(const _List& list, const int inCount = 20)
-	{
-		_Node* ptr = list.head.next;
-		int count = inCount;
-
-		while (ptr != &list.tail)
-		{
-			std::cout << inCount - count << ": " << ptr->key << " , ";
-			ptr = ptr->next;
-
-			if (--count <= 0) break;
-		}
-	}
-};
-
 namespace LIST_0_COARSE_GRAINED_SYNC // 성긴 동기화
 {
 	class Node {
@@ -152,7 +134,7 @@ namespace LIST_1_FINE_GRAINED_SYNC // 세밀한 동기화
 
 			if (key == curr->key)					// 2
 			{ 
-				curr->nodeLock.lock();				// --- 1
+				curr->nodeLock.unlock();			// --- 1
 				pred->nodeLock.unlock();			// --- 0
 				return false; 
 			}
@@ -161,7 +143,7 @@ namespace LIST_1_FINE_GRAINED_SYNC // 세밀한 동기화
 				Node* tempNode = new Node(key);
 				tempNode->next = curr;
 				pred->next = tempNode;
-				curr->nodeLock.lock();				// --- 1
+				curr->nodeLock.unlock();			// --- 1
 				pred->nodeLock.unlock();			// --- 0
 				return true;
 			}
@@ -188,16 +170,16 @@ namespace LIST_1_FINE_GRAINED_SYNC // 세밀한 동기화
 			if (key == curr->key)					// 2
 			{
 				pred->next = curr->next;
+				curr->nodeLock.unlock();			// --- 1
 				delete curr;
 
-				pred->nodeLock.unlock();			// --- 1
-				curr->nodeLock.unlock();			// --- 0
+				pred->nodeLock.unlock();			// --- 0
 				return true;
 			}
 			else									// 2
 			{
-				pred->nodeLock.unlock();			// --- 1
-				curr->nodeLock.unlock();			// --- 0
+				curr->nodeLock.unlock();			// --- 1
+				pred->nodeLock.unlock();			// --- 0
 				return false;
 			}
 		}
@@ -222,173 +204,14 @@ namespace LIST_1_FINE_GRAINED_SYNC // 세밀한 동기화
 
 			if (key == curr->key) 
 			{
-				pred->nodeLock.unlock();			// --- 1
-				curr->nodeLock.unlock();			// --- 0
+				curr->nodeLock.unlock();			// --- 1
+				pred->nodeLock.unlock();			// --- 0
 				return true;
 			}
 			else 
 			{
-				pred->nodeLock.unlock();			// --- 1
-				curr->nodeLock.unlock();			// --- 0
-				return false;
-			}
-		}
-	};
-}
-
-namespace LIST_1_FINE_GRAINED_SYNC_WithSmartLock // 세밀한 동기화
-{
-	class Node {
-	public:
-		_KeyType key;
-		Node* next;
-		std::mutex nodeLock;
-
-		Node() noexcept
-			: key()
-			, next(nullptr)
-			, nodeLock()
-		{
-		}
-
-		Node(const _KeyType keyValue) noexcept
-			: key(keyValue)
-			, next(nullptr)
-			, nodeLock()
-		{
-		}
-
-		~Node() {}
-	};
-
-	class LIST {
-	public:
-		Node head, tail;
-
-		LIST() noexcept
-			: head(INT_MIN)
-			, tail(INT_MAX)
-		{
-			head.next = &tail;
-		}
-
-		~LIST()
-		{
-			Init();
-		}
-
-		void Init()
-		{
-			Node* ptr;
-
-			while (head.next != &tail)
-			{
-				ptr = head.next;
-				head.next = head.next->next;
-				delete ptr;
-			}
-		}
-
-		bool Add(const _KeyType key)
-		{
-			Node* pred{ nullptr };
-			Node* curr{ nullptr };
-
-			std::unique_lock<std::mutex> predLock(head.nodeLock, std::deff)
-			head.nodeLock.lock();					// +++ 1
-			pred = &head;
-
-			curr = pred->next;
-			curr->nodeLock.lock();					// +++ 2
-
-			while (curr->key < key)					// 2
-			{
-				pred->nodeLock.unlock();			// --- 1
-				pred = curr;
-				curr = curr->next;
-				curr->nodeLock.lock();				// +++ 2
-			}
-
-			if (key == curr->key)					// 2
-			{
-				curr->nodeLock.lock();				// --- 1
+				curr->nodeLock.unlock();			// --- 1
 				pred->nodeLock.unlock();			// --- 0
-				return false;
-			}
-			else									// 2
-			{
-				Node* tempNode = new Node(key);
-				tempNode->next = curr;
-				pred->next = tempNode;
-				curr->nodeLock.lock();				// --- 1
-				pred->nodeLock.unlock();			// --- 0
-				return true;
-			}
-		}
-
-		bool Remove(const _KeyType key)
-		{
-			Node* pred{ nullptr };
-			Node* curr{ nullptr };
-
-			head.nodeLock.lock();					// +++ 1
-			pred = &head;
-			curr = pred->next;
-			curr->nodeLock.lock();					// +++ 2
-
-			while (curr->key < key)
-			{
-				pred->nodeLock.unlock();			// --- 1
-				pred = curr;
-				curr = curr->next;
-				curr->nodeLock.lock();				// +++ 2
-			}
-
-			if (key == curr->key)					// 2
-			{
-				pred->next = curr->next;
-				delete curr;
-
-				pred->nodeLock.unlock();			// --- 1
-				curr->nodeLock.unlock();			// --- 0
-				return true;
-			}
-			else									// 2
-			{
-				pred->nodeLock.unlock();			// --- 1
-				curr->nodeLock.unlock();			// --- 0
-				return false;
-			}
-		}
-
-		bool Contains(const _KeyType key)
-		{
-			Node* pred{ nullptr };
-			Node* curr{ nullptr };
-
-			head.nodeLock.lock();					// +++ 1
-			pred = &head;
-			curr = pred->next;
-			curr->nodeLock.lock();					// +++ 2
-
-			while (curr->key < key)
-			{
-				pred->nodeLock.unlock();			// --- 1
-				pred = curr;
-				curr = curr->next;
-				curr->nodeLock.lock();				// +++ 2
-			}
-
-			if (key == curr->key)
-			{
-				pred->nodeLock.unlock();			// --- 1
-				curr->nodeLock.unlock();			// --- 0
-				return true;
-			}
-			else
-			{
-				pred->nodeLock.unlock();			// --- 1
-				curr->nodeLock.unlock();			// --- 0
 				return false;
 			}
 		}
@@ -690,4 +513,189 @@ namespace LIST_3_LAZY_SYNC // 게으른 동기화
 			return curr->key == key && !curr->marked;
 		}
 	};
+}
+
+namespace LIST_3_LAZY_SYNC_WithSharedPtr // 게으른 동기화와 shared_ptr 사용....! 쓰레드 2개 이상일때는 문제가 됨!
+{
+	class Node {
+	public:
+		_KeyType key;
+		std::shared_ptr<Node> next;
+		std::mutex nodeLock;
+		bool marked;
+
+		Node() noexcept
+			: key()
+			, next(nullptr)
+			, nodeLock()
+			, marked(false)
+		{
+		}
+
+		Node(const _KeyType keyValue) noexcept
+			: key(keyValue)
+			, next(nullptr)
+			, nodeLock()
+			, marked(false)
+		{
+		}
+
+		~Node() = default;
+	};
+
+	class LIST {
+		using _Node = std::shared_ptr<Node>;
+
+	public:
+		_Node head;
+		_Node tail;
+
+		LIST() noexcept
+			: head(make_shared<Node>(INT_MIN))
+			, tail(make_shared<Node>(INT_MAX))
+		{
+			head->next = tail;
+		}
+
+		~LIST()
+		{
+			Init();
+		}
+
+		void Init()
+		{
+			head->next = tail;
+			//나머지는 알아서 소멸됨.
+		}
+
+		void Display()
+		{
+			_Node ptr = head->next;
+
+			for (int i = 0; i < 20; ++i)
+			{
+				std::cout << " " << ptr->key << ",";
+				ptr = ptr->next;
+
+				if (ptr == tail) break;
+			}
+
+			std::cout << "\n";
+		}
+
+		bool Validate(const _Node& const pred, const _Node& const curr)
+		{
+			return !pred->marked && !curr->marked && pred->next == curr;
+		}
+
+		bool Add(const _KeyType key)
+		{
+			_Node pred{ nullptr };
+			_Node curr{ nullptr };
+
+			pred = head;		// head는 바뀌지 않기 때문에 상관없음.
+			curr = pred->next;	// shared_ptr의 복사는 안전하지 않기 때문에, 그래서 
+
+			while (curr->key < key)
+			{
+				pred = curr;
+				curr = curr->next;
+			}
+
+			{
+				std::scoped_lock<std::mutex, std::mutex> lock(pred->nodeLock, curr->nodeLock);
+
+				if (Validate(pred, curr))
+				{
+					if (key == curr->key)
+					{
+						return false;
+					}
+					else
+					{
+						_Node tempNode = make_shared<Node>(key);
+						tempNode->next = curr;
+						pred->next = tempNode;
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+
+		bool Remove(const _KeyType key)
+		{
+			_Node pred{ nullptr };
+			_Node curr{ nullptr };
+
+			pred = head;
+			curr = pred->next;
+
+			while (curr->key < key)
+			{
+				pred = curr;
+				curr = curr->next;
+			}
+
+			{
+				std::scoped_lock<std::mutex, std::mutex> lock(pred->nodeLock, curr->nodeLock);
+
+				if (Validate(pred, curr))
+				{
+					if (key == curr->key)
+					{
+						curr->marked = true;
+						pred->next = curr->next;
+						// delete curr; // memory auto release!
+						return true;
+					}
+					else { return false; }
+				}
+				return false;
+			}
+		}
+
+		bool Contains(const _KeyType key)
+		{
+			_Node curr = head;
+			while (curr->key < key) { curr = curr->next; }
+			return curr->key == key && !curr->marked;
+		}
+	};
+}
+
+namespace CUSTOM_LIST
+{
+	template <typename _List, typename _Node>
+	void Display(const _List& list, const int inCount = 20)
+	{
+		_Node* ptr = list.head.next;
+
+		for (int i = 0; i < inCount; ++i)
+		{
+			std::cout << " " << ptr->key << ",";
+			ptr = ptr->next;
+
+			if (ptr == &list.tail) break;
+		}
+
+		std::cout << "\n";
+	}
+
+	template <>
+	void Display<LIST_3_LAZY_SYNC_WithSharedPtr::LIST, shared_ptr<LIST_3_LAZY_SYNC_WithSharedPtr::Node>>
+		(const LIST_3_LAZY_SYNC_WithSharedPtr::LIST& list, const int inCount)
+	{
+		shared_ptr<LIST_3_LAZY_SYNC_WithSharedPtr::Node> ptr = list.head->next;
+
+		for (int i = 0; i < inCount; ++i)
+		{
+			std::cout << " " << ptr->key << ",";
+			ptr = ptr->next;
+
+			if (ptr == list.tail) break;
+		}
+
+		std::cout << "\n";
+	}
 }
