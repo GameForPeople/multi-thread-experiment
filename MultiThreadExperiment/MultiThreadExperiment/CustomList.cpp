d77281eedb76f8add450506db777c74857b57d37
GameForPeople <Koreagamemaker@gmail.com>
2019-10-18 23:54:53 +0900

    Fine Grained Sync Code to cpp

--- MultiThreadExperiment/MultiThreadExperiment/CustomList.cpp
+++ MultiThreadExperiment/MultiThreadExperiment/CustomList.cpp
@@ -125,3 +125,146 @@ namespace LIST_0_COARSE_GRAINED_SYNC {
 	}
 }
 
+namespace LIST_1_FINE_GRAINED_SYNC // 세밀한 동기화
+{
+	Node::Node() noexcept
+		: key()
+		, next(nullptr)
+		, nodeLock()
+	{
+	}
+
+	Node::Node(const _KeyType keyValue) noexcept
+		: key(keyValue)
+		, next(nullptr)
+		, nodeLock()
+	{
+	}
+
+	LIST::LIST() noexcept
+		: head(INT_MIN)
+		, tail(INT_MAX)
+	{
+		head.next = &tail;
+	}
+
+	LIST::~LIST()
+	{
+		Init();
+	}
+
+	void LIST::Init()
+	{
+		Node* ptr;
+
+		while (head.next != &tail)
+		{
+			ptr = head.next;
+			head.next = head.next->next;
+			delete ptr;
+		}
+	}
+
+	bool LIST::Add(const _KeyType key)
+	{
+		Node* pred{ nullptr };
+		Node* curr{ nullptr };
+
+		head.nodeLock.lock();					// +++ 1
+		pred = &head;
+
+		curr = pred->next;
+		curr->nodeLock.lock();					// +++ 2
+
+		while (curr->key < key)					// 2
+		{
+			pred->nodeLock.unlock();			// --- 1
+			pred = curr;
+			curr = curr->next;
+			curr->nodeLock.lock();				// +++ 2
+		}
+
+		if (key == curr->key)					// 2
+		{
+			curr->nodeLock.unlock();			// --- 1
+			pred->nodeLock.unlock();			// --- 0
+			return false;
+		}
+		else									// 2
+		{
+			Node* tempNode = new Node(key);
+			tempNode->next = curr;
+			pred->next = tempNode;
+			curr->nodeLock.unlock();			// --- 1
+			pred->nodeLock.unlock();			// --- 0
+			return true;
+		}
+	}
+
+	bool LIST::Remove(const _KeyType key)
+	{
+		Node* pred{ nullptr };
+		Node* curr{ nullptr };
+
+		head.nodeLock.lock();					// +++ 1
+		pred = &head;
+		curr = pred->next;
+		curr->nodeLock.lock();					// +++ 2
+
+		while (curr->key < key)
+		{
+			pred->nodeLock.unlock();			// --- 1
+			pred = curr;
+			curr = curr->next;
+			curr->nodeLock.lock();				// +++ 2
+		}
+
+		if (key == curr->key)					// 2
+		{
+			pred->next = curr->next;
+			curr->nodeLock.unlock();			// --- 1
+			delete curr;
+
+			pred->nodeLock.unlock();			// --- 0
+			return true;
+		}
+		else									// 2
+		{
+			curr->nodeLock.unlock();			// --- 1
+			pred->nodeLock.unlock();			// --- 0
+			return false;
+		}
+	}
+
+	bool LIST::Contains(const _KeyType key)
+	{
+		Node* pred{ nullptr };
+		Node* curr{ nullptr };
+
+		head.nodeLock.lock();					// +++ 1
+		pred = &head;
+		curr = pred->next;
+		curr->nodeLock.lock();					// +++ 2
+
+		while (curr->key < key)
+		{
+			pred->nodeLock.unlock();			// --- 1
+			pred = curr;
+			curr = curr->next;
+			curr->nodeLock.lock();				// +++ 2
+		}
+
+		if (key == curr->key)
+		{
+			curr->nodeLock.unlock();			// --- 1
+			pred->nodeLock.unlock();			// --- 0
+			return true;
+		}
+		else
+		{
+			curr->nodeLock.unlock();			// --- 1
+			pred->nodeLock.unlock();			// --- 0
+			return false;
+		}
+	}
+}
--- MultiThreadExperiment/MultiThreadExperiment/CustomList.h
+++ MultiThreadExperiment/MultiThreadExperiment/CustomList.h
@@ -68,153 +68,28 @@ namespace LIST_1_FINE_GRAINED_SYNC // 세밀한 동기화
 		Node* next;
 		std::mutex nodeLock;
 
-		Node() noexcept
-			: key()
-			, next(nullptr)
-			, nodeLock()
-		{
-		}
+		Node() noexcept;
 
-		Node(const _KeyType keyValue) noexcept
-			: key(keyValue)
-			, next(nullptr)
-			, nodeLock()
-		{
-		}
+		Node(const _KeyType keyValue) noexcept;
 
-		~Node() {}
+		~Node() = default;
 	};
 
 	class LIST {
 	public:
 		Node head, tail;
 
-		LIST() noexcept
-			: head(INT_MIN)
-			, tail(INT_MAX)
-		{
-			head.next = &tail;
-		}
+		LIST() noexcept;
 
-		~LIST()
-		{
-			Init();
-		}
+		~LIST();
 
-		void Init()
-		{
-			Node* ptr;
+		void Init();
 
-			while (head.next != &tail)
-			{
-				ptr = head.next;
-				head.next = head.next->next;
-				delete ptr;
-			}
-		}
+		bool Add(const _KeyType key);
 
-		bool Add(const _KeyType key)
-		{
-			Node* pred{ nullptr };
-			Node* curr{ nullptr };
+		bool Remove(const _KeyType key);
 
-			head.nodeLock.lock();					// +++ 1
-			pred = &head;
-
-			curr = pred->next;
-			curr->nodeLock.lock();					// +++ 2
-
-			while (curr->key < key)					// 2
-			{
-				pred->nodeLock.unlock();			// --- 1
-				pred = curr;
-				curr = curr->next;
-				curr->nodeLock.lock();				// +++ 2
-			}
-
-			if (key == curr->key)					// 2
-			{ 
-				curr->nodeLock.unlock();			// --- 1
-				pred->nodeLock.unlock();			// --- 0
-				return false; 
-			}
-			else									// 2
-			{
-				Node* tempNode = new Node(key);
-				tempNode->next = curr;
-				pred->next = tempNode;
-				curr->nodeLock.unlock();			// --- 1
-				pred->nodeLock.unlock();			// --- 0
-				return true;
-			}
-		}
-
-		bool Remove(const _KeyType key)
-		{
-			Node* pred{ nullptr };
-			Node* curr{ nullptr };
-
-			head.nodeLock.lock();					// +++ 1
-			pred = &head;
-			curr = pred->next;
-			curr->nodeLock.lock();					// +++ 2
-
-			while (curr->key < key)
-			{
-				pred->nodeLock.unlock();			// --- 1
-				pred = curr;
-				curr = curr->next;					
-				curr->nodeLock.lock();				// +++ 2
-			}
-
-			if (key == curr->key)					// 2
-			{
-				pred->next = curr->next;
-				curr->nodeLock.unlock();			// --- 1
-				delete curr;
-
-				pred->nodeLock.unlock();			// --- 0
-				return true;
-			}
-			else									// 2
-			{
-				curr->nodeLock.unlock();			// --- 1
-				pred->nodeLock.unlock();			// --- 0
-				return false;
-			}
-		}
-
-		bool Contains(const _KeyType key)
-		{
-			Node* pred{ nullptr };
-			Node* curr{ nullptr };
-
-			head.nodeLock.lock();					// +++ 1
-			pred = &head;
-			curr = pred->next;
-			curr->nodeLock.lock();					// +++ 2
-
-			while (curr->key < key)
-			{
-				pred->nodeLock.unlock();			// --- 1
-				pred = curr;
-				curr = curr->next;
-				curr->nodeLock.lock();				// +++ 2
-			}
-
-			if (key == curr->key) 
-			{
-				curr->nodeLock.unlock();			// --- 1
-				pred->nodeLock.unlock();			// --- 0
-				return true;
-			}
-			else 
-			{
-				curr->nodeLock.unlock();			// --- 1
-				pred->nodeLock.unlock();			// --- 0
-				return false;
-			}
-		}
+		bool Contains(const _KeyType key);
 	};
 }
 
