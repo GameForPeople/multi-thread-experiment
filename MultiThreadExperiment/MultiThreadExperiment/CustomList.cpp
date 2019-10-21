e8ac692d3394e4f9339bae31dc8d4cdf5aaabf82
GameForPeople <koreagamemaker@gmail.com>
2019-10-21 22:50:10 +0900

    Add LockFreeSet in customList

--- MultiThreadExperiment/MultiThreadExperiment/CustomList.cpp
+++ MultiThreadExperiment/MultiThreadExperiment/CustomList.cpp
@@ -667,3 +667,209 @@ namespace LIST_3_LAZY_SYNC_WithSharedPtr
 		return curr->key == key && !curr->marked;
 	}
 }
+
+namespace LIST_4_LOCKFREE
+{
+	MarkedPointer::MarkedPointer() noexcept
+		: value()
+	{
+		Set(nullptr, false);
+	};
+
+	void MarkedPointer::Set(const Node* const node, const bool removed) noexcept
+	{
+		value = reinterpret_cast<_PointerType>(node);
+
+		value = removed
+			? value | GLOBAL::REMOVED_MASK
+			: value & GLOBAL::POINTER_MASK;
+	}
+
+	_NODISCARD Node* MarkedPointer::GetPtr() const
+	{
+		return reinterpret_cast<Node*>(value & GLOBAL::POINTER_MASK);
+	}
+
+	_NODISCARD Node* MarkedPointer::GetPtrWithRemoved(bool& removed) const
+	{
+		const auto temp = value;
+
+		0 == (temp & GLOBAL::REMOVED_MASK)
+			? removed = false
+			: removed = true;
+
+		return reinterpret_cast<Node*>(value & GLOBAL::POINTER_MASK);
+	}
+
+	bool MarkedPointer::CAS(const Node* const oldNode, const Node* const newNode, const bool oldRemoved, const bool newRemoved)
+	{
+		_PointerType oldValue = reinterpret_cast<_PointerType>(oldNode);
+
+		if (oldRemoved) oldValue = oldValue | GLOBAL::REMOVED_MASK;
+		else oldValue = oldValue & GLOBAL::POINTER_MASK;
+
+		_PointerType newValue = reinterpret_cast<_PointerType>(newNode);
+		if (newRemoved) newValue = newValue | GLOBAL::REMOVED_MASK;
+		else newValue = newValue & GLOBAL::POINTER_MASK;
+
+		return ATOMIC_UTIL::T_CAS(&value, oldValue, newValue);
+	}
+
+	bool MarkedPointer::TryMark(const Node* const oldNode, const bool newMark)
+	{
+		auto oldValue = reinterpret_cast<_PointerType>(oldNode);
+		auto newValue = oldValue;
+
+		if (newMark) newValue = newValue | GLOBAL::REMOVED_MASK;
+		else newValue = newValue & GLOBAL::POINTER_MASK;
+
+		return ATOMIC_UTIL::T_CAS(&value, oldValue, newValue);
+	}
+
+	Node::Node() noexcept
+		: key()
+		, next()
+	{
+	}
+
+	Node::Node(const _KeyType keyValue) noexcept
+		: key(keyValue)
+		, next()
+	{
+	}
+
+	List::List(const int memoryPoolSize = GLOBAL::KEY_RANGE * 2) noexcept
+		: head(INT_MIN)
+		, tail(INT_MAX)
+	{
+		head.next.Set(&tail, false);
+
+		Node* pushedNode{ nullptr };
+		for (int i = 0; i < memoryPoolSize; ++i)
+		{
+			pushedNode = new Node();
+			memoryPool.push(pushedNode);
+		}
+	}
+
+	List::~List()
+	{
+		Init();
+
+		Node* deletedNode{ nullptr };
+		bool retValue{ true };
+
+		while (retValue == false)
+		{
+			retValue = memoryPool.try_pop(deletedNode);
+			delete deletedNode;
+		}
+	}
+
+	void List::Init()
+	{
+		Node* ptr;
+
+		while (head.next.GetPtr() != &tail)
+		{
+			ptr = head.next.GetPtr();
+			head.next = head.next.GetPtr()->next;
+
+			//delete ptr;
+			memoryPool.push(ptr);
+		}
+	}
+
+	void List::Find(const _KeyType key, Node* (&pred), Node* (&curr))
+	{
+	retry:
+		while (true)
+		{
+			pred = &head;
+			curr = pred->next.GetPtr();
+
+			while (true)
+			{
+				bool removed{};
+				auto succ = curr->next.GetPtrWithRemoved(removed);
+
+				while (removed)
+				{
+					if (!(pred->next.CAS(curr, succ, false, false))) { goto retry; }
+
+					memoryPool.push(curr);
+
+					curr = succ;
+					succ = curr->next.GetPtrWithRemoved(removed);
+				}
+
+				if (curr->key >= key) return;
+
+				pred = curr;
+				curr = succ;
+			}
+		}
+	}
+
+	bool List::Add(const _KeyType key)
+	{
+		Node* pred{ nullptr };
+		Node* curr{ nullptr };
+
+		while (7)
+		{
+			Find(key, pred, curr);
+
+			if (curr->key == key) { return false; }
+			else
+			{
+				// Node* addedNode = new Node(key);
+				Node* addedNode{ nullptr };
+
+				if (!memoryPool.try_pop(addedNode))
+				{
+					addedNode = new Node(/*key*/);
+				}
+
+				addedNode->key = key;
+				addedNode->next.Set(curr, false);
+
+				if (pred->next.CAS(curr, addedNode, false, false)) { return true; }
+			}
+		}
+	}
+
+	bool List::Remove(const _KeyType key)
+	{
+		Node* pred{ nullptr };
+		Node* curr{ nullptr };
+
+		while (7)
+		{
+			Find(key, pred, curr);
+
+			if (curr->key != key) { return false; }
+			else
+			{
+				Node* nextNodeOfDeletedNode = curr->next.GetPtr();
+
+				if (!curr->next.TryMark(nextNodeOfDeletedNode, true)) { continue; }
+				if (pred->next.CAS(curr, nextNodeOfDeletedNode, false, false)) { memoryPool.push(curr); }
+
+				return true;
+			}
+		}
+	}
+
+	bool List::Contains(const _KeyType key)
+	{
+		Node* curr = &head;
+		bool removed{};
+
+		while (curr->key < key) {
+			curr = curr->next.GetPtr();
+			Node* succ = curr->next.GetPtrWithRemoved(removed);
+		}
+		return curr->key == key && !removed;
+	}
+}
--- MultiThreadExperiment/MultiThreadExperiment/CustomList.h
+++ MultiThreadExperiment/MultiThreadExperiment/CustomList.h
@@ -16,11 +16,40 @@
 namespace USING
 {
 	using _KeyType = int;
+
+#ifdef _WIN64
+	using _PointerType = long long;	// x64 64bit
+#else
+	using _PointerType = int;	// x32 32bit
+#endif
+
 }using namespace USING;
 
 namespace GLOBAL
 {
+	constexpr int NUM_TEST = 4000000;
+	constexpr int KEY_RANGE = 1000;
 
+	constexpr _PointerType REMOVED_MASK = 0x01;
+
+#ifdef _WIN64
+	constexpr _PointerType POINTER_MASK = 0xFFFFFFFFFFFFFFFE;
+#else 
+	constexpr _PointerType POINTER_MASK = 0xFFFFFFFE;
+#endif
+}
+
+namespace ATOMIC_UTIL
+{
+	template <class TYPE> bool T_CAS(std::atomic<TYPE>* addr, TYPE oldValue, TYPE newValue) noexcept
+	{
+		return atomic_compare_exchange_strong(addr, &oldValue, newValue);
+	};
+
+	template <class TYPE> bool T_CAS(volatile TYPE* addr, TYPE oldValue, TYPE newValue) noexcept
+	{
+		return atomic_compare_exchange_strong(reinterpret_cast<volatile std::atomic<TYPE>*>(addr), &oldValue, newValue);
+	};
 }
 
 namespace LIST_0_COARSE_GRAINED_SYNC // 성긴 동기화
@@ -205,6 +234,65 @@ namespace LIST_3_LAZY_SYNC_WithSharedPtr // 게으른 동기화와 shared_ptr 사용....!
 	};
 }
 
+namespace LIST_4_LOCKFREE // 락 프리.
+{
+	class Node;
+
+	class MarkedPointer
+	{
+		_PointerType value;	// next Node Pointer with removed Mark(1bit).
+	public:
+		MarkedPointer() noexcept;
+
+		void Set(const Node* const node, const bool removed) noexcept;
+
+		_NODISCARD Node* GetPtr() const;
+
+		_NODISCARD Node* GetPtrWithRemoved(bool& removed) const;
+
+		bool CAS(const Node* const oldNode, const Node* const newNode, const bool oldRemoved, const bool newRemoved);
+
+		bool TryMark(const Node* const oldNode, const bool newMark);
+	};
+
+	class Node
+	{
+	public:
+		_KeyType key;
+		MarkedPointer next;
+
+		Node() noexcept;
+		Node(const _KeyType keyValue) noexcept;
+		~Node() = default;
+	};
+
+	class List {
+	public:
+		Node head, tail;
+		concurrent_queue<Node*> memoryPool;
+
+		List(const int memoryPoolSize /* = GLOBAL::KEY_RANGE * 2 */) noexcept;
+
+		~List();
+
+		void Init();
+
+		// Validate를 따로하지않음 의미가없음... 내가 락을 걸어놨으니까 Validate하는 건데, 락을 걸지 않는 락프리 자료구조에서는 의미가없음.
+		//bool Validate(const Node* const pred, const Node* const curr)
+		//{
+		//	return !pred->marked && !curr->marked && pred->next == curr;
+		//}
+
+		void Find(const _KeyType key, Node* (&pred), Node* (&curr));
+
+		bool Add(const _KeyType key);
+
+		bool Remove(const _KeyType key);
+
+		bool Contains(const _KeyType key);
+	};
+}
+
 namespace CUSTOM_LIST
 {
 	template <typename _List, typename _Node>
@@ -235,6 +323,22 @@ namespace CUSTOM_LIST
 			ptr = ptr->next;
 
 			if (ptr == list.tail) break;
+		}
+
+		std::cout << "\n";
+	}
+
+	template <>
+	void Display<LIST_4_LOCKFREE::List, LIST_4_LOCKFREE::Node>(const LIST_4_LOCKFREE::List& list, const int inCount)
+	{
+		LIST_4_LOCKFREE::Node* ptr = list.head.next.GetPtr();
+
+		for (int i = 0; i < inCount; ++i)
+		{
+			if (ptr == &list.tail) break;
+
+			std::cout << " " << ptr->key << ",";
+			ptr = ptr->next.GetPtr();
 		}
 
 		std::cout << "\n";
